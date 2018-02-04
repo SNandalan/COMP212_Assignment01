88a3ccd7398baca7c461f585aa7cfd80780b0d13
SNandalan <sushmitanandalan@gmail.com>
2018-02-03 20:51:56 -0500

    Created a functional Temperature Conversion GUI that converts Farenheit to Celsius.

---
+++ TemperatureConversion/TempGUI.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TemperatureConversion
+{
+    public partial class TempGUI : Form
+    {
+        public TempGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CelsiusValue_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double FVal = double.Parse(FValue.Text);
+            double Celsius = (5.0 / 9.0) * (FVal - 32);
+            CelsiusValue.Text = Celsius.ToString();
+        }
+    }
+}
